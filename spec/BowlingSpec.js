dc07f1b06f89dbd80f93b0beac58592593a45582
Bart <kluczynskib@gmail.com>
2016-08-07 19:58:59 +0200

    the test for calculating the bonus does not pass

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -12,16 +12,40 @@ describe ('A bowling game', function() {
 
   it('calculates the score after the first roll', function() {
     bowling.roll = function() { return 5; };
-    var result = bowling.roll();
-    expect(bowling.getScoreAfterRoll()).toEqual(10 - result);
+    var result = bowling.playBowling();
+    expect(bowling.playBowling()).toEqual(10 - result);
   });
 
   it('calculates the pins available after the first roll', function() {
     bowling.roll = function() { return 3; };
-    var result = bowling.roll();
+    var result = bowling.playBowling();
     expect(bowling.getCurrentPinsAvailable()).toEqual(10 - result);
 
   });
+
+  it('shows the current roll in the frame', function(){
+    bowling.playBowling();
+    expect(bowling.getCurrentRoll()).toEqual(2);
+  });
+
+  it('shows the current frame in the game', function(){
+    for (var i = 0; i < 9; i++) {
+      bowling.playBowling();
+    }
+    expect(bowling.getCurrentFrame()).toEqual(5);
+  });
+
+  it ('calculates the bonus for STRIKE correctly', function(){
+    bowling.roll = function(){ return 10; };
+    var result1 = bowling.playBowling();
+    var result2 = bowling.playBowling();
+    expect(bowling.getCurrentScore()).toEqual(10 + 2 * result1 + 2 * result2)
+  });
+
+
+
+
+
 
 
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -3,6 +3,9 @@
 function Bowling(){
   this.score = 0;
   this.pinsAvailable = 10;
+  this.currentRoll = 1;
+  this.currentFrame = 1;
+  this.framesHistory = [];
 }
 
 Bowling.prototype.getCurrentScore = function(){
@@ -12,13 +15,47 @@ Bowling.prototype.getCurrentScore = function(){
 Bowling.prototype.roll = function(){
   return Math.floor((Math.random() * this.pinsAvailable) + 1);
 }
-Bowling.prototype.getScoreAfterRoll = function(){
+Bowling.prototype.playBowling = function(){
   var roll = this.roll();
   this.pinsAvailable -= roll;
   this.score += roll;
+  this.currentRoll++;
+  this.framesHistory.push(roll);
+  this.processBonus(roll);
+  this.updateCurrentFrame();
+  this.resetThePins();
+  this.resetTheRolls();
   return roll;
 }
 Bowling.prototype.getCurrentPinsAvailable = function(){
-  this.getScoreAfterRoll();
   return this.pinsAvailable;
+}
+Bowling.prototype.getCurrentRoll = function(){
+  return this.currentRoll;
+}
+
+Bowling.prototype.updateCurrentFrame = function(){
+  if (this.currentRoll === 3){
+    this.currentFrame++;
+  }
+}
+
+Bowling.prototype.getCurrentFrame = function(){
+  return this.currentFrame;
+}
+Bowling.prototype.resetThePins = function(){
+  if (this.currentRoll === 3){
+    this.pinsAvailable = 10
+  }
+}
+Bowling.prototype.resetTheRolls = function(){
+  if (this.currentRoll === 3){
+    this.currentRoll = 1
+  }
+}
+
+Bowling.prototype.processBonus = function(roll){
+  if (this.currentRoll === 1 && roll === 10){
+    this.score += this.framesHistory[this.currentFrame + 1];
+  }
 }
